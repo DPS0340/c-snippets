5d3837995f939c9ffcc1103d482c64eb15b29d31
jiho lee <jiholee@kpu.ac.kr>
2020-03-21 05:39:44 +0900

    warning fix && .gitignore add && seed.c newline printf

--- src/randomseed/seed.c
+++ src/randomseed/seed.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int get_int(const char*);
 
 int file_write_int(int, const char*, const char*);
 
 int main(void) {
-    int write_val = get_int("주입할 랜덤 시드를 입력해 주세요.");
+    int write_val = get_int("주입할 랜덤 시드를 입력해 주세요.\n");
 
     int res = file_write_int(write_val, "seed", "w");
 
